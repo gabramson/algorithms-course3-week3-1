015ae99c14ab29d4db26377b3c345084c3f9caac
Gary Abramson <gary.abramson@gmail.com>
2017-10-19 19:36:07 -0500

    Test written

--- HuffmanTest/HuffmanTest.cs
+++ HuffmanTest/HuffmanTest.cs
@@ -8,6 +8,15 @@ namespace HuffmanTest
         [TestMethod]
         public void TestMethod1()
         {
+            var encoder = new Encoder();
+            encoder.AddSymbol(32);
+            encoder.AddSymbol(25);
+            encoder.AddSymbol(20);
+            encoder.AddSymbol(18);
+            encoder.AddSymbol(5);
+            encoder.Encode();
+            Assert.AreEqual(4, encoder.MaxWordLength);
+            Assert.AreEqual(2, encoder.MaxWordLength);
         }
     }
 }
