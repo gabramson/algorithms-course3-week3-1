a1649049e0187777f6a1c88ff382a9259b371db4
Gary Abramson <gary.abramson@gmail.com>
2017-10-20 00:06:03 -0500

    Refactoring to make intent clearer

--- HuffmanLib/Encoder.cs
+++ HuffmanLib/Encoder.cs
@@ -6,13 +6,13 @@ namespace HuffmanLib
 {
     public class Encoder
     {
-        private SortedList<int, (int,int)> frequencies = new SortedList<int, (int,int)>();
+        private SortedList<int, MinMax> frequencies = new SortedList<int, MinMax>();
 
         public int MinWordLength
         {
             get
             {
-                return frequencies.Values[0].Item1;
+                return frequencies.Values[0].Min;
             }
         }
 
@@ -20,50 +20,41 @@ namespace HuffmanLib
         {
             get
             {
-                return frequencies.Values[0].Item2;
+                return frequencies.Values[0].Max;
             }
         }
 
-
         public void AddSymbol(int weight)
         {
-            frequencies.Add(weight, (0,0));
+            frequencies.Add(weight, new MinMax { Min = 0, Max = 0 });
         }
 
         public void Encode() { 
             while (frequencies.Count > 1)
             {
                 int newFrequency = frequencies.Keys[0] + frequencies.Keys[1];
-                int newMin = Min(frequencies.Values[0].Item1, frequencies.Values[1].Item1) + 1;
-                int newMax = Max(frequencies.Values[0].Item2, frequencies.Values[1].Item2) + 1;
+                int newMin = Min(frequencies.Values[0].Min, frequencies.Values[1].Min) + 1;
+                int newMax = Max(frequencies.Values[0].Max, frequencies.Values[1].Max) + 1;
                 frequencies.RemoveAt(0);
                 frequencies.RemoveAt(0);
-                frequencies.Add(newFrequency, (newMin, newMax));
+                frequencies.Add(newFrequency, new MinMax { Min = newMin, Max = newMax });
             }
         }
 
         private int Max(int first, int second)
         {
-            if (first > second)
-            {
-                return first;
-            }
-            else
-            {
-                return second;
-            }
+            return (first > second) ? first : second;
         }
 
         private int Min(int first, int second)
         {
-            if (first < second)
-            {
-                return first;
-            }
-            else
-            {
-                return second;
-            }
+            return (first < second) ? first : second;
+        }
+
+        private struct MinMax
+        {
+            public int Min { get; set; } 
+            public int Max { get; set; } 
         }
     }
 }
